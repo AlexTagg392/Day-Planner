ceebe18bb9ff069d19dd050cb233e9fa91b2956a
aTagg <christag392@gmail.com>
2020-06-02 19:51:05 -0400

    finished

--- README.md
+++ README.md
@@ -11,16 +11,44 @@
     7. License
 
 ## Description:
-   
+   I was given the following User Story to take and develop a program to fit the user's needs:
+
+    AS AN employee with a busy schedule
+    
+    I WANT to add important events to a daily planner
+    
+    SO THAT I can manage my time effectively
+
 
 ## Installation:
     No installation needed
 
 ## Usage:
-    
+    This is a very useful program where any user can use it to schedule out their day.
 
 ## Contributing:
-   These instructions were given from the Vanderbilt Coding Bootcamp from the University and Trilogy
+   These instructions were given from the Vanderbilt Coding Bootcamp from the University and Trilogy. They are as follows:
+    
+    GIVEN I am using a daily planner to create a schedule
+
+    WHEN I open the planner
+    THEN the current day is displayed at the top of the calendar
+
+    WHEN I scroll down
+    THEN I am presented with timeblocks for standard business hours
+
+    WHEN I view the timeblocks for that day
+    THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+    WHEN I click into a timeblock
+    THEN I can enter an event
+
+    WHEN I click the save button for that timeblock
+    THEN the text for that event is saved in local storage 
+
+    WHEN I refresh the page
+    THEN the saved events persist
+
     
 
 ## Credits:
---
+++ script.js
@@ -0,0 +1,178 @@
+$(document).ready(function () {
+  const test = false;
+  const now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+  let nowHour24 = moment().format('H');
+  let nowHour12 = moment().format('h');
+
+  if (test) {
+    nowHour24 = 13;
+    nowHour12 = 1;
+  }
+
+  let $dateHeading = $('#navbar-subtitle');
+  $dateHeading.text(now);
+
+  const saveIcon = "./images/save-regular.svg";
+
+  // Get stored todos from localStorage
+  let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+  if (test) { 
+    // console.log(storedPlans); 
+  }
+
+  // If plans were retrieved from localStorage, update the plan array to it
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    planTextArr = new Array(9);
+  }
+
+  if (test) { 
+    // console.log("full array of plned text", planTextArr); 
+  }
+
+  let $plannerDiv = $('#plannerContainer');
+  $plannerDiv.empty();
+
+  if (test) { 
+    // console.log("current time", nowHour12); 
+  }
+
+
+  // Calendar by hour
+  for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
+    let $rowDiv = $('<div>');
+
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index', hour);
+
+    // timebox
+    let $col2TimeDiv = $('<div>');
+    $col2TimeDiv.addClass('col-md-2');
+
+    const $timeBoxSpn = $('<span>');
+    $timeBoxSpn.attr('class', 'timeBox');
+
+    // display hours
+    let displayHour = 0;
+    let ampm = "";
+
+    if (hour > 12) {
+      displayHour = hour - 12;
+      ampm = "pm";
+    } else {
+      displayHour = hour;
+      ampm = "am";
+    }
+    $timeBoxSpn.text(`${displayHour} ${ampm}`);
+
+    $rowDiv.append($col2TimeDiv);
+    $col2TimeDiv.append($timeBoxSpn);
+
+    // input row
+    let $dailyPlanSpn = $('<input>');
+
+    $dailyPlanSpn.attr('id', `input-${index}`);
+    $dailyPlanSpn.attr('hour-index', index);
+    $dailyPlanSpn.attr('type', 'text');
+    $dailyPlanSpn.attr('class', 'dailyPlan');
+
+    $dailyPlanSpn.val(planTextArr[index]);
+
+    let $col9IptDiv = $('<div>');
+    $col9IptDiv.addClass('col-md-9');
+
+    $rowDiv.append($col9IptDiv);
+    $col9IptDiv.append($dailyPlanSpn);
+ 
+
+    // Save row
+    let $col1SaveDiv = $('<div>');
+    $col1SaveDiv.addClass('col-md-1');
+
+    let $saveBtn = $('<i>');
+    $saveBtn.attr('id', `saveid-${index}`);
+    $saveBtn.attr('save-id', index);
+    $saveBtn.attr('class', "far fa-save saveIcon");
+
+    $rowDiv.append($col1SaveDiv);
+    $col1SaveDiv.append($saveBtn);
+
+    // Row color on Time
+    updateRowColor($rowDiv, hour);
+    $plannerDiv.append($rowDiv);
+  };
+
+  //Updating Row Color
+  function updateRowColor($hourRow, hour) {
+
+    if (test) { 
+      // console.log("rowColor ", nowHour24, hour); 
+    }
+    if (hour < nowHour24) {
+      if (test) { 
+        // console.log("lessThan"); 
+      }
+      $hourRow.css("background-color", "lightgrey")
+    } else if (hour > nowHour24) {
+      if (test) { 
+        // console.log("greaterthan"); 
+      }
+      $hourRow.css("background-color", "lightgreen")
+    } else {
+      if (test) { 
+        // console.log("eqaul"); 
+      }
+      $hourRow.css("background-color", "tomato")
+    }
+  };
+
+  // Local Storage
+  $(document).on('click', 'i', function (event) {
+    event.preventDefault();
+
+    if (test) { 
+      // console.log('click pta before ' + planTextArr); 
+    }
+
+    let $index = $(this).attr('save-id');
+    let inputId = '#input-' + $index;
+    let $value = $(inputId).val();
+
+    planTextArr[$index] = $value;
+
+
+    if (test) { 
+      // console.log('value ', $value); 
+    }
+
+    if (test) { 
+      // console.log('index ', $index); 
+    }
+
+    if (test) { 
+      // console.log('click pta after ' + planTextArr); 
+    }
+
+    localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+  });
+
+  // Color Save Button
+  $(document).on('change', 'input', function (event) {
+    event.preventDefault();
+    if (test) { 
+      // console.log('onChange'); 
+    }
+
+    if (test) { 
+      // console.log('id', $(this).attr('hour-index')); 
+    }
+
+    let i = $(this).attr('hour-index');
+
+  });
+});
